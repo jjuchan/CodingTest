be4c9797036e134a98de64685b27af445b828013
jjuchan <yjc7241@naver.com>
2024-08-21 00:50:03 +0900

    테스트 문제들

---
+++ backjon/test10926.java
@@ -0,0 +1,11 @@
+package backjon;
+
+import java.util.Scanner;
+
+public class test10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine();
+        System.out.println(A+"??!");
+    }
+}

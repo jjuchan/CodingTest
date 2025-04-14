ed38301e8856a786c406a22721a2903cb2768127
jjuchan <yjc7241@naver.com>
2025-04-14 19:29:09 +0900

    문제 풀이

--- backjon/test1546.java
+++ backjon/test1546.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class test1546 {
     public static void main(String[] args) {
+        /* 1번째 풀이
         Scanner sc = new Scanner(System.in);
         int subjects = sc.nextInt();
         int arr[] = new int[subjects];
@@ -23,6 +24,22 @@ public class test1546 {
         System.out.println(ave);
 
         sc.close();
+        */
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        for(int i =0; i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        long sum = 0;
+        long max = 0;
+
+        for(int i =0; i<arr.length;i++){
+            if(arr[i]>max) max = arr[i];
+            sum += arr[i];
+        }
+        System.out.println(sum*100.0/max/N);
         }
     }
 

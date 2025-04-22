56331c9d11c67283b1811c6383fdcf5d2e1d6472
jjuchan <yjc7241@naver.com>
2025-04-22 16:42:27 +0900

    문제 풀이

---
+++ backjon/test1920.java
@@ -0,0 +1,40 @@
+package backjon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class test1920 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N  = sc.nextInt(); // 정렬할 수
+        int[] A = new int[N]; //정렬할 배열 선언
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        Arrays.sort(A);
+        int M = sc.nextInt(); //탐색할 숫자의 갯수
+        for(int i = 0; i < M; i++){
+            boolean find = false;
+            int target = sc.nextInt();
+            int start = 0;
+            int end = A.length-1;
+            while(start <= end){
+                int mid_index = (start + end) / 2;
+                int mid_value = A[mid_index];
+                if(target < mid_value){
+                    end = mid_index - 1;
+                } else if (target > mid_value) {
+                    start = mid_index + 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+            if (find) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
---
+++ backjon/test2178.java
@@ -0,0 +1,54 @@
+package backjon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class test2178 {
+    static int[] dx = {0,1,0,-1}; //수학이랑 다른 개념 y가 오른쪽으로 가면 1
+    static int[] dy = {1,0,-1,0}; //x가 내려가면 1
+    static boolean[][] visited;
+    static int[][] A;
+    static int N,M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); //행의 갯수
+        M = Integer.parseInt(st.nextToken()); //열의 갯수
+        visited = new boolean[N][M];
+        A = new int[N][M];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken(); //ex)1010110
+            for(int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(line.substring(j, j+1));
+            }
+        }
+        BFS(0,0);
+        System.out.println(A[N-1][M-1]); //0부터 시작하기 때문에 1을 뺀 인덱스를 출력해야 마지막 값이 나옴
+    }
+
+    private static void BFS(int i, int j) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{i, j});
+        while (!q.isEmpty()) {
+            int now[] = q.poll();
+            visited[i][j] = true;
+            for(int k = 0; k < 4; k++) { //상하좌우 탐색이라 4
+                int x = now[0] + dx[k]; //행
+                int y = now[1] + dy[k]; //열
+                if(x>=0 && x<N && y>=0 && y<M ) { //배열을 넘어가면 안되고
+                    if (A[x][y] != 0 && !visited[x][y]) { // 0이여서 갈 수 없거나 방문한적 있으면 안됨
+                        //갈 수 있는 곳
+                        visited[x][y] = true;
+                        A[x][y] = A[now[0]][now[1]] + 1; //더해주는 과정 depth
+                        q.add(new int[]{x, y});
+                    }
+                }
+            }
+        }
+    }
+}

386ab51272e52165dde59bfc6362e2b1b220fbdb
jjuchan <yjc7241@naver.com>
2025-05-16 15:33:29 +0900

    문제 풀이

---
+++ backjon/test15688.java
@@ -0,0 +1,29 @@
+package backjon;
+
+
+import java.io.*;
+import java.util.Arrays;
+
+public class test15688 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);  // 배열 정렬
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : arr) {
+            sb.append(num).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
+

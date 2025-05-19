36172711766de601a18ca2e6f376d015a5999012
jjuchan <yjc7241@naver.com>
2025-05-19 22:54:52 +0900

    문제 풀이

---
+++ backjon/test11656.java
@@ -0,0 +1,22 @@
+package backjon;
+
+import java.util.*;
+import java.io.*;
+public class test11656 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < S.length(); i++) {
+            result.add(S.substring(i));
+        }
+        Collections.sort(result);
+        for (String line : result) {
+            System.out.println(line);
+        }
+    }
+}

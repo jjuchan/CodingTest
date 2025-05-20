3e1ab861c7b224bdef5834e51fe554ab7ed860b5
jjuchan <yjc7241@naver.com>
2025-05-20 20:52:01 +0900

    문제 풀이

---
+++ backjon/test2193.java
@@ -0,0 +1,22 @@
+package backjon;
+
+import java.io.*;
+
+public class test2193 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[91]; // int로는 부족하므로 long으로
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[N]);
+    }
+}

37736a71c0e033b1246ffaf2f64007c438781185
jjuchan <yjc7241@naver.com>
2025-05-21 15:11:01 +0900

    문제 풀이

---
+++ backjon/test1912.java
@@ -0,0 +1,30 @@
+package backjon;
+
+import java.io.*;
+import java.util.*;
+
+public class test1912 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n]; // dp[i] = i번째까지 연속합 중 최대
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        int max = dp[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}

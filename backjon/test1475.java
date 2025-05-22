299c4c8c381e8d0267ca9efbf209fea97907a70c
jjuchan <yjc7241@naver.com>
2025-05-22 23:20:40 +0900

    백준 1475 기본 구현 문제 풀이

---
+++ backjon/test1475.java
@@ -0,0 +1,32 @@
+package backjon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class test1475 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] count = new int[10];
+
+        String N = br.readLine();
+
+        for (int i = 0; i < N.length(); i++) {
+            int num = N.charAt(i) - '0'; // 문자 → 숫자 변환
+            if (num == 6 || num == 9) {
+                count[6]++; // 6과 9는 같은 그룹으로 취급
+            } else {
+                count[num]++;
+            }
+        }
+        count[6] = (count[6] + count[9]) / 2;
+        int max = 0;
+        for (int i = 0; i < 9; i++) {
+            max = Math.max(max, count[i]);
+        }
+
+        System.out.println(max);
+    }
+}

8f37eb497bff24fbb43c047f49616be170753051
jjuchan <yjc7241@naver.com>
2025-05-22 15:21:37 +0900

    문제 풀이

---
+++ programmers/day4/practice1.java
@@ -0,0 +1,32 @@
+package programmers.day4;
+
+import java.util.Arrays;
+
+import java.util.*;
+import java.io.*;
+public class practice1 {
+    public static void main(String[] args) {
+        int[] number = {6, 10, 2};
+        System.out.println(solution(number));
+
+
+    }
+
+    public static String solution(int[] numbers) {
+
+        String[] line = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            line[i] = String.valueOf(numbers[i]); //받아온 배열의 수를 문자열로 바꾸어 저장
+        }
+        Arrays.sort(line, (a, b) -> (b + a).compareTo(a + b)); //b + a면 큰 수가 앞에
+
+        if (line[0].equals("0")) return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for (String a : line) {
+            sb.append(a);
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
---
+++ programmers/day4/practice2.java
@@ -0,0 +1,4 @@
+package programmers.day4;
+
+public class practice2 {
+}

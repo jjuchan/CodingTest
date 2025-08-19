f9d629ed08fdc64406ccc057c61241f214468a0a
jjuchan <yjc7241@naver.com>
2025-08-19 21:19:22 +0900

    practice 1 add

---
+++ programmers/day6/practice1.java
@@ -0,0 +1,24 @@
+package programmers.day6;
+
+public class practice1 {
+    public static void main(String[] args) {
+
+        int a = 7;
+        Solution s = new Solution();
+        System.out.println(s.solution(a));
+
+    }
+
+   static class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            if (n % 7 == 0) {
+                answer = n / 7;
+            } else {
+                answer = n / 7 + 1;
+            }
+
+            return answer;
+        }
+    }
+}

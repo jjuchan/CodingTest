6281a7dbb74d87d105391e3a139472857b7e220e
jjuchan <yjc7241@naver.com>
2025-08-20 16:36:37 +0900

    PG_76501

---
+++ programmers/day7/PG_76501.java
@@ -0,0 +1,24 @@
+package programmers.day7;
+
+public class PG_76501 {
+    public static void main(String[] args) {
+
+    }
+
+
+    class Solution {
+        public int solution(int[] absolutes, boolean[] signs) {
+            int sum = 0;
+            for (int i = 0; i < absolutes.length; i++) {
+                if (signs[i]) {
+                    sum += absolutes[i];
+                } else {
+                    sum -= absolutes[i];
+                }
+            }
+
+            int answer = sum;
+            return answer;
+        }
+    }
+}

4e50b6de8dac8fefc3f2fea4d423147200522526
jjuchan <yjc7241@naver.com>
2025-08-21 20:56:42 +0900

    PG_12910

---
+++ programmers/PG_12910.java
@@ -0,0 +1,29 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
+public class PG_12910 {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int[] solution(int[] arr, int divisor) {
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int i : arr) {
+                if (i % divisor == 0) {
+                    list.add(i);
+                }
+            }
+            if (list.isEmpty()) {
+                list.add(-1);
+            }
+           Collections.sort(list);
+
+            return list.stream().mapToInt(i -> i).toArray();
+        }
+    }
+}

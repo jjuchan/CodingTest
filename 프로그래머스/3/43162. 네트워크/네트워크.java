a6fd597fe0ba2e1d8a286e066bdad3d38f45f2dd
윤주찬 <163832764+jjuchan@users.noreply.github.com>
2025-12-21 10:24:24 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int networkCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, computers, visited);
+                networkCount++;
+            }
+        }
+
+        return networkCount;
+    }
+
+    private void dfs(int current, int[][] computers, boolean[] visited) {
+        visited[current] = true;
+
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[current][i] == 1 && !visited[i]) {
+                dfs(i, computers, visited);
+            }
+        }
+    }
+}

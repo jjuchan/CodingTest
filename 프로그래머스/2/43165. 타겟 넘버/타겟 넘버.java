0869def5c397623109d8a2800f18f9fa66bdc68a
윤주찬 <163832764+jjuchan@users.noreply.github.com>
2025-12-21 10:24:14 +0900

    [level 2] Title: 타겟 넘버, Time: 9.42 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,24 @@
+class Solution {
+    int [] numbers;
+    int target;
+    int answer;
+    
+    void dfs (int index, int sum){
+        if(index == numbers.length){
+            if(sum == target)answer++;
+            return;
+        }
+        
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    
+    public int solution(int[] numbers, int target) { //target이 나오는 경우의 수 
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+        
+        dfs(0,0);
+        return answer;
+    }
+}

c34b34a793849ad94755b00a87f163651495a391
윤주찬 <163832764+jjuchan@users.noreply.github.com>
2026-01-18 09:22:21 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 376 ms, Memory: 37068 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int[] arr1 = new int[A];
+            int[] arr2 = new int[B];
+
+            st = new StringTokenizer(br.readLine());
+            for (int a = 0; a < A; a++) {
+                arr1[a] = Integer.parseInt(st.nextToken());
+            }
+            st = new StringTokenizer(br.readLine());
+            for (int b = 0; b < B; b++) {
+                arr2[b] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr2);  // arr2 정렬
+
+            int count = 0;
+            for (int j = 0; j < arr1.length; j++) {
+                count += find(arr1[j], arr2);
+            }
+            System.out.println(count);
+
+        }
+    }
+
+    // arr 배열에서 target보다 작은 값의 개수 리턴 (이분탐색)
+    public static int find(int target, int[] arr) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}

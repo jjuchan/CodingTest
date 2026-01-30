bcb584884d691febac35e586b0a9b672d00136f6
윤주찬 <163832764+jjuchan@users.noreply.github.com>
2026-01-30 15:09:12 +0900

    [Silver IV] Title: 제곱근, Time: 292 ms, Memory: 26652 KB -BaekjoonHub

---
+++ 백준/Silver/13706. 제곱근/제곱근.java
@@ -0,0 +1,30 @@
+
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger N = sc.nextBigInteger();
+
+        BigInteger start = BigInteger.ONE;
+        BigInteger end = N;
+
+        while (start.compareTo(end) <= 0) {
+            BigInteger mid = (start.add(end)).divide(BigInteger.TWO);
+            BigInteger answer = mid.pow(2);
+            int result = answer.compareTo(N);
+
+            if (result == 0) {
+                System.out.println(mid);
+                return;
+            } else if (result < 0) {
+                start = mid.add(BigInteger.ONE);
+            } else {
+                end = mid.subtract(BigInteger.ONE); //수가 너무 크면 작아져야 하니까 마지막 값을 줄여줌
+            }
+        }
+
+    }
+}

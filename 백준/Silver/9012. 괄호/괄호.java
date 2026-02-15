88998c44a2770197b7e95ea5127053e0baa81cf7
윤주찬 <163832764+jjuchan@users.noreply.github.com>
2026-02-15 12:28:04 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            String s = br.readLine();
+            Stack<Character> stack = new Stack<>();
+            boolean isVPS = true;
+
+            for (int j = 0; j < s.length(); j++) {
+                char ch = s.charAt(j);
+
+                if (ch == '(') {
+                    stack.push(ch);
+                } else if (ch == ')') {
+                    if (stack.isEmpty()) {
+                        isVPS = false;
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            // 전체 문자열 다 돌고 난 뒤에 검사해야 함
+            if (!stack.isEmpty()) {
+                isVPS = false;
+            }
+
+            System.out.println(isVPS ? "YES" : "NO");
+        }
+    }
+}

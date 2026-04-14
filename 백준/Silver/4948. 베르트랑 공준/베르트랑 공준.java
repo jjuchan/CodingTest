abc4dc9a2059b01d12973c7f00414fb460f434f7
윤주찬 <163832764+jjuchan@users.noreply.github.com>
2026-04-14 15:07:21 +0900

    [Silver II] Title: 베르트랑 공준, Time: 844 ms, Memory: 14644 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			int n = Integer.parseInt(br.readLine());
+			if (n == 0) break;
+			
+			int count = 0;
+			
+			for (int i = n + 1; i < 2 * n + 1; i++) {
+				if (isPrime(i)) count++;
+			}
+			System.out.println(count);
+		}
+	}
+	
+	static  boolean isPrime(int n) {
+		if (n == 1) return false;
+		for(int i =2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) return false;
+		}
+		return true;
+	}
+}

7ff32a049b22c415c245c751bec04d18bf3bf759
윤주찬 <163832764+jjuchan@users.noreply.github.com>
2026-04-14 15:39:00 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22828 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] arr;
+	static boolean[] visited;
+	static StringBuilder stringBuilder = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		stringBuilder = new StringBuilder();
+
+		arr = new int[M];
+
+		visited = new boolean[N + 1];
+
+		dfs(0);
+
+		System.out.println(stringBuilder);
+
+	}
+
+	static void dfs(int depth) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				stringBuilder.append(arr[i]).append(" ");
+			}
+			stringBuilder.append("\n");
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i;
+				dfs(depth + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
